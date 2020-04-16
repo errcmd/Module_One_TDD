9e6571ddff89a9f5d6285b6f6b03b821ec7979f6
Oleg Lavrushchik <oleg.lavrushchik@appxite.com>
2020-04-16 14:40:29 +0300

    dirty way

---
+++ CalculatorSample/CalculatorSample.Logic/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSample.Logic
+{
+    public class Calculator
+    {
+        private ILogger _logger;
+
+        public Calculator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public int Plus(int x, int y)
+        {
+            _logger.Log($"Operation Plus: x ={x}, y={y}");
+            return x + y;
+        }
+
+        public int Minus(int x, int y)
+        {
+            _logger.Log($"Operation Minus: x ={x}, y={y}");
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            _logger.Log($"Operation Multiply: x ={x}, y={y}");
+            return x * y;
+        }
+
+        public double Divide(int x, int y)
+        {
+            _logger.Log($"Operation Degree: x ={x}, y={y}");
+            return (double)x / (double)y;
+        }
+        public double Degree(int x, int y)
+        {
+            _logger.Log($"Operation Degree: x ={x}, y={y}");
+            return Math.Pow(x, y);
+        }
+
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.AppendAllText("log.txt", message);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
---
+++ CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
@@ -0,0 +1,116 @@
+﻿using CalculatorSample.Logic;
+using Moq;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace CalculatorSample.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Test_Plus_Positive_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Plus(3, 4);
+            mock.Verify(l => l.Log(It.IsAny<string>()));
+            Assert.AreEqual(expected: 7, actual);
+        }
+        [Test]
+        public void Test_Plus_Negative_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Plus(-3, -4);
+            Assert.AreEqual(expected: -7, actual);
+        }
+
+        [Test]
+        public void Test_Minus_Positive_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Minus(5, 4);
+            mock.Verify(l => l.Log(It.IsAny<string>()));
+            Assert.AreEqual(expected: 1, actual);
+        }
+
+        [Test]
+        public void Test_Minus_Negative_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Minus(-3, -4);
+            Assert.AreEqual(expected: 1, actual);
+        }
+
+        [Test]
+        public void Test_Multiply_Positive_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Multiply(2, 3);
+            mock.Verify(l => l.Log(It.IsAny<string>()));
+            Assert.AreEqual(expected: 6, actual);
+        }
+
+        [Test]
+        public void Test_Multuply_Negative_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Multiply(-2, 3);
+            Assert.AreEqual(expected: -6, actual);
+        }
+
+        [Test]
+        public void Test_Divide_Positive_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Divide(5, 2);
+            mock.Verify(l => l.Log(It.IsAny<string>()));
+            Assert.AreEqual(expected: 2.5, actual);
+        }
+
+        [Test]
+        public void Test_Divide_Negative_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Divide(-9, -2);
+            Assert.AreEqual(expected: 4.5, actual);
+        }
+
+        [Test]
+        public void Test_Degree_Positive_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Degree(2, 2);
+            mock.Verify(l => l.Log(It.IsAny<string>()));
+            Assert.AreEqual(expected: 4, actual);
+        }
+
+        [Test]
+        public void Test_Degree_Negative_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Degree(-2, 3);
+            Assert.AreEqual(expected: -8, actual);
+        }
+
+    }
+}
